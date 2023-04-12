9deea4b83f09d4974c981e54c4416ce75d44ac44
OrangeChick <retww@naver.com>
2023-04-12 12:55:19 +0900

    Feat: Implement Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import { twMerge } from 'tailwind-merge';
+import HeaderTop from './HeaderTop';
+import HeaderNav from './HeaderNav';
+
+export interface HeaderProps {
+  type: 'guest' | 'member';
+  className?: string;
+}
+
+export default function Image({ type, className, ...props }: HeaderProps) {
+  return (
+    <div className={twMerge(`font-line-seed-sans-kr`, className)} {...props}>
+      <HeaderTop type={type} />
+      <HeaderNav type={type} />
+    </div>
+  );
+}
