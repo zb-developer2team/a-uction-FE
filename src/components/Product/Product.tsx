ac681e6c3bd22694c1f02a506e9c3a87a0a95734
Yuni0221 <moonrise002002@gmail.com>
2023-04-12 17:39:26 +0900

    Feat: Implement Main Product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -15,32 +15,17 @@ export default function Product({
   className,
 }: ProductProps) {
   return (
-    <div
-      className={twMerge(
-        `w-[240px] h-[369.5px] font-line-seed-sans-kr`,
-        className
-      )}
-    >
+    <div className={twMerge(`w-[240px] font-line-seed-sans-kr`, className)}>
       <Image
         size={'sm'}
         src={'/src/assets/SampleImage.png'}
         alt={'상품 이미지 입니다.'}
       />
       <div className="ml-2">
-        <h1
-          className={twMerge(
-            `absolute text-lg mt-1 font-bold drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]`
-          )}
-        >
-          A+uction 제품
-        </h1>
-        <p className="absolute mt-10 text-Gray text-base font-bold">
-          {category}
-        </p>
-        <p className="absolute mt-[72px] text-base font-bold">
-          시작가 : {startPrice}원
-        </p>
-        <p className="absolute mt-[102px] text-base font-bold">
+        <h2 className={twMerge(`text-lg mt-1 font-bold`)}>A+uction 제품</h2>
+        <p className="text-Gray text-base font-bold">{category}</p>
+        <p className="text-base font-bold">시작가 : {startPrice}원</p>
+        <p className="text-base font-bold">
           현재가 : <span className="text-Red">{price}원</span>
         </p>
       </div>
