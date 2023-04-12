b50519fb4e897e27455275ad04cd1bb58f2388b8
OrangeChick <retww@naver.com>
2023-04-12 13:16:25 +0900

    Fix: Fix Button component
    - className은 필수로 사용되지 않기에 옵셔널로 변경하였습니다.
    - type 이름을 'abled' 에서 'active'로 변경하였습니다.
    - size에서 basic 크기는 사용하지 않기에 제거하였습니다.
    - className으로 바로 활용할 수 있는 color는 type에서 제거하였습니다.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import { twMerge } from 'tailwind-merge';
-import tailwindConfig from '../../../tailwind.config';
 
 export interface ButtonProps {
-  className: string;
+  className?: string;
   label: string;
-  type: 'abled' | 'disabled';
-  size: 'basic' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
-  color: 'Orange' | 'Red' | 'Bluegreen';
+  type: 'active' | 'disabled';
+  size: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
   onClick: () => void;
 }
 
@@ -16,7 +13,6 @@ export default function Button({
   label,
   type,
   size,
-  color,
   onClick,
   ...props
 }: ButtonProps) {
@@ -28,7 +24,6 @@ export default function Button({
   };
 
   const buttonSize = {
-    basic: 'w-[150px] h-[60px]',
     xsmall: 'w-[80px] h-[30px]',
     small: 'w-[120px] h-[50px]',
     medium: 'w-[360px] h-[40px]',
@@ -36,29 +31,20 @@ export default function Button({
     xlarge: 'w-[600px] h-[80px]',
   };
 
-  const buttonColor = {
-    Orange:
-      'bg-Orange hover:bg-White hover:text-Orange hover:border-2 border-Orange',
-    Red: 'bg-Red hover:bg-White hover:text-Red hover:border-2 border-Red',
-    Bluegreen:
-      'bg-Bluegreen hover:bg-White hover:text-Bluegreen hover:border-2 border-Bluegreen',
+  const style = {
+    active:
+      'font-bold text-base text-White rounded-lg font-line-seed-sans-kr bg-Orange',
+    disabled:
+      'bg-LightGray text-DarkGray text-base font-bold font-line-seed-sans-kr cursor-not-allowed pointer-events-none rounded-lg ',
   };
 
   return (
-    <div className={twMerge(`${buttonSize[size]}`, className)}>
-      <button
-        className={twMerge(
-          `${buttonSize[size]} font-bold text-base text-white rounded-lg font-line-seed-sans-kr`,
-          buttonColor[color],
-          className,
-          isDisabled
-            ? 'bg-LightGray text-DarkGray text-base font-bold font-line-seed-sans-kr cursor-not-allowed pointer-events-none'
-            : ''
-        )}
-        onClick={handleClick}
-      >
-        {label}
-      </button>
-    </div>
+    <button
+      className={twMerge(`${buttonSize[size]} ${style[type]}`, className)}
+      onClick={handleClick}
+      {...props}
+    >
+      {label}
+    </button>
   );
 }
