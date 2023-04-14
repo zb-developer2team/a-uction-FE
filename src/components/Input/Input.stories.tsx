c6c377650169fe28aff516e10d33e82f362ef618
OrangeChick <retww@naver.com>
2023-04-14 15:56:17 +0900

    Feat: Add Input stories
    - 시작가와 최소 응찰가격 story를 추가하였습니다.

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -21,18 +21,21 @@ export const Email = Template.bind({});
 Email.args = {
   type: 'text',
   placeholder: 'email',
+  imageType: 'email',
 };
 
 export const Name = Template.bind({});
 Name.args = {
   type: 'text',
   placeholder: 'name',
+  imageType: 'name',
 };
 
 export const Number = Template.bind({});
 Number.args = {
   type: 'text',
   placeholder: 'number',
+  imageType: 'number',
 };
 
 export const Password: StoryFn<typeof Input> = (args) => (
@@ -42,29 +45,54 @@ export const Password: StoryFn<typeof Input> = (args) => (
 Password.args = {
   type: 'text',
   placeholder: 'password',
+  imageType: 'password',
 };
 
 export const PasswordCheck = Password.bind({});
 PasswordCheck.args = {
   type: 'text',
   placeholder: 'passwordCheck',
+  imageType: 'password',
 };
 
 const Search: StoryFn<typeof Input> = (args) => (
-  <Input className="border-2 rounded-lg" {...args} />
+  <Input className="border-2 rounded-lg pl-[10px]" {...args} />
 );
 
 export const SearchProduct = Search.bind({});
 SearchProduct.args = {
   type: 'search',
   placeholder: 'searchProduct',
+  imageType: 'none',
 };
 
 export const ProductName: StoryFn<typeof Input> = (args) => (
-  <Input className="border-2 rounded-lg" {...args} />
+  <Input className="border-2 rounded-lg pl-[10px]" {...args} />
 );
 
 ProductName.args = {
   type: 'text',
   placeholder: 'productName',
+  imageType: 'none',
+};
+
+const Price: StoryFn<typeof Input> = (args) => (
+  <div className="flex items-center">
+    <Input className="w-[300px] border-2 rounded-lg pl-[10px]" {...args} />{' '}
+    <span className="font-line-seed-sans-kr text-lg ml-[10px]">원</span>
+  </div>
+);
+
+export const StartPrice = Price.bind({});
+StartPrice.args = {
+  type: 'text',
+  placeholder: 'startPrice',
+  imageType: 'none',
+};
+
+export const Bidding = Price.bind({});
+Bidding.args = {
+  type: 'text',
+  placeholder: 'bidding',
+  imageType: 'none',
 };
