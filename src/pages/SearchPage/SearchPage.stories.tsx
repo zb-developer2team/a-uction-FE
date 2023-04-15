5c036c92fb469a0fc4417b028e0f6ef0578325c1
Yuni0221 <moonrise002002@gmail.com>
2023-04-15 20:00:39 +0900

    Feat: [Feat/#75/SearchPage] Implement Search Page

---
+++ src/pages/SearchPage/SearchPage.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import SearchPage from './SearchPage';
+
+const meta = {
+  component: SearchPage,
+  title: 'pages/SearchPage',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'figmaURL',
+    },
+  },
+} as Meta<typeof SearchPage>;
+
+export default meta;
+
+const Template: StoryFn<typeof SearchPage> = (args) => <SearchPage {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  count: '34',
+};
