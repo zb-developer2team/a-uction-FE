213614ab4a5a302f4295bf5c71105bc1f0e1595e
OrangeChick <retww@naver.com>
2023-04-21 01:44:01 +0900

    Feat: Implement Login validation

--- src/pages/Login/components/LoginForm/LoginForm.tsx
+++ src/pages/Login/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { twMerge } from 'tailwind-merge';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Input from '../../../../components/Input/Input';
 import Button from '../../../../components/Button/Button';
 import TextLink from '../../../../components/TextLink/TextLink';
@@ -12,22 +12,76 @@ export interface LoginFormProps {
 export default function LoginForm({ className, ...props }: LoginFormProps) {
   const [buttonIsActive, setButtonState] = useState(true);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [emailMsg, setEmailMsg] = useState('');
+  const [passwordMsg, setPasswordMsg] = useState('');
+
   function clickHandler() {
     setButtonState(false);
   }
 
+  const validateEmail = (email: string) => {
+    return email
+      .toLowerCase()
+      .match(
+        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+      );
+  };
+
+  const validatePwd = (password: string) => {
+    return password.toLowerCase().match(/^[a-z|A-Z|0-9|]{6,12}$/);
+  };
+
+  const isEmailValid = validateEmail(email);
+  const isPwdValid = validatePwd(password);
+
+  const isAllValid = isEmailValid && isPwdValid;
+
+  const onChangeEmail = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      const currentEmail = e.target.value;
+
+      setEmail(currentEmail);
+
+      if (!validateEmail(currentEmail)) {
+        setEmailMsg('이메일 형식으로 입력해주세요.');
+      } else {
+        setEmailMsg('');
+      }
+    },
+    []
+  );
+
+  const onChangePwd = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const currentPassword = e.target.value;
+
+    setPassword(currentPassword);
+
+    if (!validatePwd(currentPassword)) {
+      setPasswordMsg('6~12자리 영문과 숫자를 입력해주세요.');
+    } else {
+      setPasswordMsg('');
+    }
+  }, []);
+
   return (
     <form className="flex-col justify-center border-b-2 border-LightGray">
-      <div>
+      <div className="relative">
         <label className={twMerge('sr-only')}>이메일</label>
         <Input
           type={'text'}
           placeholder={'email'}
           imageType={'email'}
-          className="mb-10"
+          className="mb-[50px]"
+          onChange={onChangeEmail}
         />
+        <p className={twMerge('mt-[5px] mb-5 pl-5 text-Red absolute bottom-0')}>
+          {emailMsg}
+        </p>
       </div>
-      <div>
+      <div className="relative">
         <label className={twMerge('sr-only')}>비밀번호</label>
         <Input
           type={'text'}
@@ -36,11 +90,15 @@ export default function LoginForm({ className, ...props }: LoginFormProps) {
           maxLength={12}
           minLength={6}
           className="mb-[50px]"
+          onChange={onChangePwd}
         />
+        <p className="mt-[5px] mb-5 pl-5 text-Red absolute bottom-0">
+          {passwordMsg}
+        </p>
       </div>
       <Link to="/">
         <Button
-          type={'active'}
+          type={!isAllValid ? 'disabled' : 'active'}
           label={'로그인'}
           size={'medium'}
           onClick={clickHandler}
