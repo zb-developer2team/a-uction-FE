730bcefd949f56e777c136208c0d82ecd8fd36c3
Yuni0221 <moonrise002002@gmail.com>
2023-04-20 16:43:36 +0900

    Enhancement: [Enhancement/#110] Enhancement component for product regist

--- src/components/MinBeedingPrice/MinBeedingPrice.tsx
+++ src/components/MinBeedingPrice/MinBeedingPrice.tsx
@@ -3,9 +3,18 @@ import Input from '../../components/Input/Input';
 
 export interface MinBeedingPriceProps {
   className?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function MinBeedingPrice({ className }: MinBeedingPriceProps) {
+export default function MinBeedingPrice({
+  className,
+  onChange,
+}: MinBeedingPriceProps) {
+  const handleBiddingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const price = Number(event.target.value);
+    console.log('Input 값이 변경되었습니다:', event.target.value);
+    onChange && onChange(event);
+  };
   return (
     <>
       <div className={twMerge(`flex h-[74px] border-b-[1px] mt-6`)}>
@@ -15,6 +24,7 @@ export default function MinBeedingPrice({ className }: MinBeedingPriceProps) {
           placeholder={'bidding'}
           imageType={'none'}
           className="border-2 rounded-lg"
+          onChange={handleBiddingChange}
         />
         <span className="text-xl mt-1 ml-4">원</span>
       </div>
