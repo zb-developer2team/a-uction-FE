edd1c02246b80a0943d64da3e71f577b9e7dcbff
Yuni0221 <moonrise002002@gmail.com>
2023-04-21 16:39:10 +0900

    Enhancement: [Enhancement/#122/Dropdown] Enhancement Dropdown/ Product Category component

--- src/components/ProductCategory/ProductCategory.tsx
+++ src/components/ProductCategory/ProductCategory.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from 'tailwind-merge';
+import { useState } from 'react';
 import Dropdown from '../../components/Dropdown/Dropdown';
 
 export interface ProductCategoryProps {
@@ -6,11 +7,22 @@ export interface ProductCategoryProps {
 }
 
 export default function Category({ className }: ProductCategoryProps) {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleDropdownClick = (item: string) => {
+    setSelectedCategory(item);
+    console.log('Selected Category:', item);
+  };
+
   return (
     <>
       <div className={twMerge(`flex h-[74px] border-b-[1px] mt-6`)}>
         <span className="text-xl font-bold mt-1 mr-10">카테고리</span>
-        <Dropdown type={'product'} className="z-50" />
+        <Dropdown
+          type={'product'}
+          className="z-50"
+          onDropdownClick={handleDropdownClick}
+        />
       </div>
     </>
   );
