7bef6e0fbf6d102ccc55a75ee5cf1b00b2c1bf12
Yuni0221 <moonrise002002@gmail.com>
2023-04-23 18:05:25 +0900

    Enhancement: [Enhancement/#130/DateTimePicker] Enhance Date Time Picker

--- src/components/EndDateTime/EndDateTime.tsx
+++ src/components/EndDateTime/EndDateTime.tsx
@@ -1,19 +1,37 @@
 import { twMerge } from 'tailwind-merge';
-import DateTimePicker from '../../components/DateTimePicker/DateTimePicker';
+import { Dayjs } from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { useState } from 'react';
 
 export interface EndDateTimeProps {
   className?: string;
+  onChange?: (date: string) => void;
 }
 
-export default function EndDateTime({ className }: EndDateTimeProps) {
+export default function EndDateTime({ onChange, className }: EndDateTimeProps) {
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const handleChange = (Date: Dayjs | null) => {
+    if (Date !== null) {
+      const dateToSendToAPI = Date.toISOString();
+      setSelectedDate(dateToSendToAPI);
+      if (onChange) {
+        onChange(dateToSendToAPI);
+      }
+    } else {
+      setSelectedDate('');
+    }
+  };
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className={twMerge(`flex h-[74px] border-b-[1px] mt-6`)}>
         <span className="text-xl font-bold mt-1 mr-10">경매종료시간</span>
         <div className="-mt-3">
-          <DateTimePicker />
+          <DateTimePicker onChange={handleChange} />
         </div>
       </div>
-    </>
+    </LocalizationProvider>
   );
 }
