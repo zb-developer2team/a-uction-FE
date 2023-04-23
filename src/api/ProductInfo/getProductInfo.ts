cb904be016914b5c907a59edecde1aaec2bd2a98
Yuni0221 <moonrise002002@gmail.com>
2023-04-23 21:56:36 +0900

    Config :[Config/#136] Config and Add Library

--- src/api/ProductInfo/getProductInfo.ts
+++ src/api/ProductInfo/getProductInfo.ts
@@ -14,14 +14,19 @@ const getProductInfo = async (
   token: string,
   productInfo: PostProductInfo
 ): Promise<PostProductInfo> => {
-  return await axios({
-    method: 'post',
-    url: `http://localhost:8000/api/auctions`,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    data: { productInfo },
-  });
+  try {
+    const response = await axios({
+      method: 'post',
+      url: `http://localhost:8000/api/auctions`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { productInfo },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('실패');
+  }
 };
-
 export default getProductInfo;
