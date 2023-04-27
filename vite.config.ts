252439b6e9a547f4926116fd7857706ce33f1a50
OrangeChick <retww@naver.com>
2023-04-27 12:41:56 +0900

    Fix: Remove unnecessary code
    - 불필요한 환경변수 관련 코드를 제거하였습니다.

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,4 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env': {
-      VITE_IMAGE_PATH: './src/assets',
-    },
-  },
 });
