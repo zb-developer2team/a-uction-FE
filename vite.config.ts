d21595b9ce17e4b3f93c8e3848f071137e95ea93
OrangeChick <retww@naver.com>
2023-04-27 15:34:45 +0900

    Chore: Add proxy code to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.35.38.11:8081',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
