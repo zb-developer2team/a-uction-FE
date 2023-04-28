435cb5577bcbe0ca25e4d39bba3aab77530c81b7
Yuni0221 <moonrise002002@gmail.com>
2023-04-28 13:47:49 +0900

    Enhancement: [Enhancement/#182] Enhancement Product / Search Page

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 
 export interface ProductProps {
+  itemName: string;
   category: string;
   startPrice: string;
   price: string;
@@ -11,6 +12,7 @@ export interface ProductProps {
 }
 
 export default function Product({
+  itemName,
   category,
   startPrice,
   price,
@@ -30,7 +32,7 @@ export default function Product({
           alt={'상품 이미지 입니다.'}
         />
         <div className="ml-2">
-          <h2 className={twMerge(`text-lg mt-1 font-bold`)}>A+uction 제품</h2>
+          <h2 className={twMerge(`text-base mt-1 font-bold`)}>{itemName}</h2>
           <p className="text-Gray text-base font-bold">{category}</p>
           <p className="text-base font-bold">시작가 : {startPrice}원</p>
           <p className="text-base font-bold">
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,41 +1,142 @@
+import axios from 'axios';
 import { twMerge } from 'tailwind-merge';
 import Header from '../../components/Header/Header';
 import Dropdown from '../../components/Dropdown/Dropdown';
 import Product from '../../components/Product/Product';
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 export interface SearchPageProps {
-  count?: string;
   className?: string;
 }
 
-const products = [
-  { category: '미술품', startPrice: '50,000', price: '60,000' },
-  { category: '가구', startPrice: '100,000', price: '120,000' },
-  { category: '디지털', startPrice: '200,000', price: '250,000' },
-  { category: '의류', startPrice: '30,000', price: '35,000' },
+export interface ProductData {
+  itemName: string;
+  category: string;
+  startPrice: string;
+  price: string;
+}
+
+const dummyProducts: ProductData[] = [
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '5,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '120,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '5,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '120,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '5,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '120,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '5,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '120,000',
+  },
+  {
+    itemName: 'Apple 정품 아이폰 14 Pro 자급제',
+    category: 'ETC',
+    startPrice: '5000',
+    price: '5,000',
+  },
 ];
 
-export default function SearchPage({ count, className }: SearchPageProps) {
+export default function SearchPage({ className }: SearchPageProps) {
+  const [products, setProducts] = useState<ProductData[]>([]);
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // const response = await axios.get('/auctions/my-auctions');
+      // setProducts(response.data);
+
+      setProducts(dummyProducts);
+      setCount(dummyProducts.length);
+    };
+    fetchData();
+  }, []);
+
+  const productRows: ProductData[][] = [];
+  let currentRow: ProductData[] = [];
+  for (let i = 0; i < products.length; i++) {
+    currentRow.push(products[i]);
+    if (currentRow.length === 4 || i === products.length - 1) {
+      productRows.push(currentRow);
+      currentRow = [];
+    }
+  }
+
   return (
     <div>
       <Header type={'guest'} />
       <div className="text-3xl font-bold mt-[100px] ml-[50px]">
         총 {count}건이 검색되었습니다.
       </div>
-      <div className="flex justify-end mr-[50px]">
-        <Dropdown type={'sort'} />
-      </div>
-
-      <div className="flex justify-around mt-10 mb-10">
-        {products.map((product, index) => (
-          <Product
-            key={index}
-            category={product.category}
-            startPrice={product.startPrice}
-            price={product.price}
-          />
-        ))}
+      <div className="relative">
+        <div
+          className={twMerge(
+            `flex justify-end absolute mt-10 right-0 mr-20 z-50`
+          )}
+        >
+          <Dropdown type={'sort'} />
+        </div>
+        <div className="flex absolute justify-evenly flex-wrap ml-24 mt-32 mb-10">
+          {productRows.map((row, rowIndex) => (
+            <div className="flex w-full z-1" key={rowIndex}>
+              {row.map((product, index) => (
+                <div
+                  className={twMerge(
+                    `w-full sm:w-1/2 md:w-1/4 mb-16 ${
+                      index !== row.length - 1 ? 'sm:mr-2' : ''
+                    }`
+                  )}
+                  key={`product-${index}`}
+                >
+                  <Product
+                    key={index}
+                    itemName={product.itemName}
+                    category={product.category}
+                    startPrice={product.startPrice}
+                    price={product.price}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
