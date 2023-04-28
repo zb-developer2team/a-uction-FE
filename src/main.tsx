f5cc049e6df470431516b3effb595419180556a4
OrangeChick <retww@naver.com>
2023-04-28 12:32:44 +0900

    Feat: Add RecoilRoot to apply global state

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import MyBiddingPage from './pages/MyBidding/MyBidding';
 import DepositPage from './pages/DepositPage/DepositPage';
 import Preparing from './pages/Preparing/Preparing';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 export var token =
   'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyRW1haWwiOiJtYXN0ZXIiLCJzdWIiOiJBY2Nlc3NUb2tlbiIsImlhdCI6MTY4MTg4MTE2NCwiZXhwIjoxNjgzMDkwNzY0fQ.WTVSTVsxVi8NNlhiENVJgqNNqSO96LA9TDrAe8Bymys';
@@ -20,26 +21,28 @@ export var token =
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/regist" element={<Regist />} />
-          <Route path="/detail" element={<ProductDetailPage />} />
-          <Route path="/mybidding" element={<MyBiddingPage />} />
-          <Route path="/deposit" element={<DepositPage />} />
-          <Route path="/progression" element={<SearchPage />} />
-          <Route path="/upcoming" element={<Preparing />} />
-          <Route path="/result" element={<SearchPage />} />
-          <Route path="/preparing" element={<Preparing />} />
-          <Route path="/inform" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
-  </QueryClientProvider>
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/regist" element={<Regist />} />
+            <Route path="/detail" element={<ProductDetailPage />} />
+            <Route path="/mybidding" element={<MyBiddingPage />} />
+            <Route path="/deposit" element={<DepositPage />} />
+            <Route path="/progression" element={<SearchPage />} />
+            <Route path="/upcoming" element={<Preparing />} />
+            <Route path="/result" element={<SearchPage />} />
+            <Route path="/preparing" element={<Preparing />} />
+            <Route path="/inform" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    </QueryClientProvider>
+  </RecoilRoot>
 );
