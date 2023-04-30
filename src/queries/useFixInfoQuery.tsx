906185f5bb0dc1ff8041f31c197bcfe6eda91724
Yuni0221 <moonrise002002@gmail.com>
2023-05-01 00:12:26 +0900

    Fix the error3

--- src/queries/useFixInfoQuery.tsx
+++ src/queries/useFixInfoQuery.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import getFixInfo, { PostFixInfo } from '../api/ProductInfo/getFixInfo';
 
 export function useFixInfoQuery(token: string, FixInfo: PostFixInfo) {
