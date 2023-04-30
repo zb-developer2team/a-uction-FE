7e237811ab33c7aba04432cbc0082b9ea1f4bc3c
Yuni0221 <moonrise002002@gmail.com>
2023-04-30 11:27:12 +0900

    Fix: [Fix/#190] Fix the Header title

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -13,12 +13,12 @@ export default function HeaderNav({
   ...props
 }: HeaderNavProps) {
   const navigation = {
-    guest: ['홈', '진행 중인 경매', '예정 경매', '경매 결과', '경매 안내'],
+    guest: ['홈', '진행 중인 경매', '예정 경매', '지난 경매', '경매 안내'],
     member: [
       '홈',
       '진행 중인 경매',
       '예정 경매',
-      '경매 결과',
+      '지난 경매',
       '경매 안내',
       '마이페이지',
       '물품 등록',
@@ -36,7 +36,7 @@ export default function HeaderNav({
       <NavLink to="/">홈</NavLink>
       <NavLink to="/progression">진행 중인 경매</NavLink>
       <NavLink to="/upcoming">예정 경매</NavLink>
-      <NavLink to="/result">경매 결과</NavLink>
+      <NavLink to="/result">지난 경매</NavLink>
       <NavLink to="/inform">경매 안내</NavLink>
       {type === 'member' ? <NavLink to="/mypage">마이페이지</NavLink> : ''}
       {type === 'member' ? <NavLink to="/regist">물품 등록</NavLink> : ''}
